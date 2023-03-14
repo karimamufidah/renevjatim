3d3566e789bfd5a43ddf52c7ae995e1e54a2a944
Muslip Laptop bw015 <muszczlim@gmail.com>
2023-03-15 00:24:51 +0700

    REST API IBT

---
+++ application/models/utilities/Get_ibt_realisasi_for_insert_m.php
@@ -0,0 +1,19 @@
+<?php
+class Get_ibt_realisasi_for_insert_m extends CI_Model
+{
+  private function _get_table_name()
+  {
+    return "ibt_realisasi AS main";
+  }
+
+  public function show($filters)
+  {
+    $this->db->where("tanggal", $filters->tanggal);
+    $this->db->where("ibt", $filters->nama);
+
+    $query = $this->db->get($this->_get_table_name());
+    $result = $query->result();
+
+    return count($result) ? $result[0] : null;
+  }
+}
